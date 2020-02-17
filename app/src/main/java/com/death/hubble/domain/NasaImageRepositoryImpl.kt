d2fd0491ab5df53a446eac835bf5b89664e92450
carotkut94 <carotkut12@gmail.com>
2020-02-17 14:37:56 +0530

    added mocikto for testing via creating mock objects

--- app/src/main/java/com/death/hubble/domain/NasaImageRepositoryImpl.kt
+++ app/src/main/java/com/death/hubble/domain/NasaImageRepositoryImpl.kt
@@ -4,6 +4,8 @@ import com.death.hubble.data.NasaImage
 import com.death.hubble.network.NetworkService
 import io.reactivex.Single
 
+
+
 class NasaImageRepositoryImpl(private val networkService: NetworkService) : NasaImageRepository{
     override fun getNasaPhotos(token: String): Single<List<NasaImage>> = networkService.getTrendingRepositories(token)
 }
